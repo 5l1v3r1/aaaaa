50d80c2620538e465dcbbe00964be6165c94226c
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-14 15:39:07 +0600

    Update hiddencam.sh

--- a/hiddencam.sh
+++ a/hiddencam.sh
@@ -32,14 +32,14 @@ exit 1
 banner() {
 
 clear
-printf "\e[1;33m  _    _ _     _     _             \e[1;31m  _____                 \e[0m\n"
-printf "\e[1;33m | |  | (_)   | |   | |            \e[1;31m / ____|                \e[0m\n"
-printf "\e[1;33m | |__| |_  __| | __| | ___ _ __   \e[1;31m| |     __ _ _ __ ___   \e[0m\n"
-printf "\e[1;33m |  __  | |/ _  |/ _  |/ _ \  _ \  \e[1;31m| |    / _  |  _   _ \  \e[0m\n"
-printf "\e[1;33m | |  | | | (_| | (_| |  __/ | | | \e[1;31m| |___| (_| | | | | | | \e[0m\n"
-printf "\e[1;33m |_|  |_|_|\__,_|\__,_|\___|_| |_| \e[1;31m \_____\__,_|_| |_| |_| \e[0m\n"
+printf "\e[1;33m  _    _ _     _     _             \e[1;32m  _____                 \e[0m\n"
+printf "\e[1;33m | |  | (_)   | |   | |            \e[1;32m / ____|                \e[0m\n"
+printf "\e[1;33m | |__| |_  __| | __| | ___ _ __   \e[1;32m| |     __ _ _ __ ___   \e[0m\n"
+printf "\e[1;33m |  __  | |/ _  |/ _  |/ _ \  _ \  \e[1;32m| |    / _  |  _   _ \  \e[0m\n"
+printf "\e[1;33m | |  | | | (_| | (_| |  __/ | | | \e[1;32m| |___| (_| | | | | | | \e[0m\n"
+printf "\e[1;33m |_|  |_|_|\__,_|\__,_|\___|_| |_| \e[1;32m \_____\__,_|_| |_| |_| \e[0m\n"
 printf " \n"
-printf " \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by htr-tech (tahmid.rayat)\e[0m\n"
+printf "    \e[92m[\e[37;1m+\e[92m]\e[0m\e[36;1m Tool Created by htr-tech (tahmid.rayat)\e[0m\n"
 printf " \n"
 
 }
