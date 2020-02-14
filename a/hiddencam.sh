7e4c80c6126194ab79b78117d11de1149db09d2e
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-14 15:32:46 +0600

    Update hiddencam.sh

--- a/hiddencam.sh
+++ a/hiddencam.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+trap 'printf "\n";stop' 2
 req() {
 
 command -v php > /dev/null 2>&1 || { echo >&2 "PHP is not installed ! Install it."; exit 1; }
@@ -7,6 +8,25 @@ command -v curl > /dev/null 2>&1 || { echo >&2 "Curl is not installed ! Install 
 command -v ssh > /dev/null 2>&1 || { echo >&2 "Openssh is not installed ! Install it"; exit 1; }
 command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzip is not installed ! Install it"; exit 1; }
 command -v python2 > /dev/null 2>&1 || { echo >&2 "Python2 is not installed ! Install it"; exit 1; }
+
+}
+stop() {
+
+checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+checkssh=$(ps aux | grep -o "ssh" | head -n1)
+if [[ $checkngrok == *'ngrok'* ]]; then
+pkill -f -2 ngrok > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+fi
+
+if [[ $checkphp == *'php'* ]]; then
+killall -2 php > /dev/null 2>&1
+fi
+if [[ $checkssh == *'ssh'* ]]; then
+killall -2 ssh > /dev/null 2>&1
+fi
+exit 1
 
 }
 banner() {
@@ -24,3 +44,5 @@ printf " \n"
 
 }
 
+banner
+req
