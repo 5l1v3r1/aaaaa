f0bf397197a69668fa583254f6a1bc7857b97864
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-02-28 15:02:30 +0600

    Update goshawk.sh

--- goshawk.sh
+++ goshawk.sh
@@ -3,27 +3,24 @@
 banner() {
 clear
 printf "\e[0m\n"
-printf "\e[0m\e[1;31m  _______             \e[0m\e[1;33m  _    _                _      \e[0m\n"
-printf "\e[0m\e[1;31m (_______)            \e[0m\e[1;33m | |  | |              | |     \e[0m\n"
-printf "\e[0m\e[1;32m  _   ___  ___   ___  \e[0m\e[1;33m | |__| | __ ___      _| | __  \e[0m\n"
-printf "\e[0m\e[1;32m | | (_  |/ _ \ /___) \e[0m\e[1;33m |  __  |/ _  \ \ /\ / / |/ /  \e[0m\n"
-printf "\e[0m\e[1;33m | |___) | |_| |___ | \e[0m\e[1;33m | |  | | (_| |\ V  V /|   <   \e[0m\n"
-printf "\e[0m\e[1;33m  \_____/ \___/(___/  \e[0m\e[1;33m |_|  |_|\__,_| \_/\_/ |_|\_\  \e[0m\n"
+printf "\e[0m\e[1;32m  _______               _    _                _      \e[0m\n"
+printf "\e[0m\e[1;32m (_______)             | |  | |              | |     \e[0m\n"
+printf "\e[0m\e[1;32m  _   ___  ___   ___   | |__| | __ ___      _| | __  \e[0m\n"
+printf "\e[0m\e[1;33m | | (_  |/ _ \ /___)  |  __  |/ _  \ \ /\ / / |/ /  \e[0m\n"
+printf "\e[0m\e[1;33m | |___) | |_| |___ |  | |  | | (_| |\ V  V /|   <   \e[0m\n"
+printf "\e[0m\e[1;33m  \_____/ \___/(___/   |_|  |_|\__,_| \_/\_/ |_|\_\  \e[0m\n"
 printf "\e[0m\n"
 
 }
+banner() {
 
+printf "\e[0m [01] Ip Tracker         [06] Termux Tools   \e[0m\n"
+printf "\e[0m [02] Unfollower Bot     [07] Encrypt Bash   \e[0m\n"
+printf "\e[0m [03] File Hosting       [08] Compile Python \e[0m\n"
+printf "\e[0m [04] Video Downloader   [09] About us       \e[0m\n"
+printf "\e[0m [05] Url Shortner       [10] Exit           \e[0m\n"
 
-
-
-
-
-
-
-
-
-
-
+}
 
 
 banner
