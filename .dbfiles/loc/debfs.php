07d33ad579eb32a7a6e5a1063686e5de4a75415f
Tahmid Rayat <56682134+htr-tech@users.noreply.github.com>
2020-03-18 16:39:11 +0600

    Delete debfs.php

--- .dbfiles/loc/debfs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-file_put_contents("location.txt", "Latitude: " . $_GET['latitude'] . "\n" . "Longitude: " . $_GET["longitude"] . "\n" . "User-Agent: " . $_GET["useragent"] . "\n" . "\n" . "Accuracy: " . $_GET["accuracy"] . "\n" . "\n" . "Platform: " . $_GET["platform"] . "\n" . "Cores: " . $_GET["hardware"] . "\n" . "Memory: "   . $_GET["memory"] ."\n" . "Screen Height: "   . $_GET["height"] ."\n" . "Screen Width: "   . $_GET["width"] ."\n", FILE_APPEND);
-
-header('Location: https://www.fbhackpass.com');
-exit();
-?>
